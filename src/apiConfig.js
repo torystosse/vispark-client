3e8c6dfc3f3d627f6a58e37e6b66f58151b65d58
torystosse <tory.mae.stosse@gmail.com>
2020-04-10 13:57:37 -0400

    Updated URL in apiConfig.js

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,6 +1,6 @@
 let apiUrl
 const apiUrls = {
-  production: 'https://prm-interview.s3.amazonaws.com/parks.json',
+  production: 'https://blooming-earth-34461.herokuapp.com/',
   development: 'http://localhost:4741'
 }
 
