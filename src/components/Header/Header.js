b5cf48e364aaf752c727f0e469d83f61e9b92824
torystosse <tory.mae.stosse@gmail.com>
2020-04-10 13:26:58 -0400

    Added image to navbar, changed color
    Added mountain image to navbar in header.js. Removed 'primary' bg
    on Navbar

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -9,7 +9,8 @@ const alwaysOptions = (
 )
 
 const Header = ({ user }) => (
-  <Navbar bg="primary" variant="dark" expand="md">
+  <Navbar variant="dark" expand="md" className="nav-bar">
+    <img src='http://clipart-library.com/images_k/mountain-clipart-transparent-background/mountain-clipart-transparent-background-14.png' className="mountain-image" />
     <Navbar.Brand href="#">
       Vispark
     </Navbar.Brand>
